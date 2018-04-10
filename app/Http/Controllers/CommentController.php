d7581aae779d4404d5a0c593f8713fc120623364
Aronne Brivio <aronne.brivio@studenti.unimi.it>
2018-04-10 17:15:50 +0200

    testCommentsNotExistingPost OK

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Comment;
+use App\Post;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
@@ -10,12 +11,15 @@ class CommentController extends Controller
     public function getAll(Request $request)
     {
         $post_id = $request->all()['post_id'];
-        return Comment::where('post_id', $post_id)->get();
+        $post = Post::find($post_id);
+        if($post)
+            return Comment::where('post_id', $post_id)->get();
+
+        return response('Post not found', 404);
     }
 
     public function new(Request $request)
     {
-        /* insert */
         $comment = new Comment;
         $comment->fill($request->all());
         $comment->save();
@@ -24,7 +28,6 @@ class CommentController extends Controller
     public function update(Request $request, $id)
     {
         $comment = Comment::find($id);
-        /* update */
         $comment->fill($request->all());
         $comment->save();
 
