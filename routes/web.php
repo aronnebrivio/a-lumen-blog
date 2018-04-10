0b2f0781c480f2940c22bcd4808253622aa455c9
Aronne Brivio <aronne.brivio@studenti.unimi.it>
2018-04-10 15:52:36 +0200

    Refactoring: using $route$router->group()

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,24 @@
 <?php
 
-use App\Http\Controllers\ExampleController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\PostController;
 use App\Http\Controllers\CommentController;
-
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
+use App\Http\Controllers\ExampleController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 
 $router->get('/', ExampleController::class . '@test');
 
-/* get */
-$router->get('/users', UserController::class . '@getAll');
-$router->get('/user/{id}', UserController::class . '@get');
+$router->group(['prefix' => 'users'], function () use ($router) {
+    $router->get('/', UserController::class . '@getAll');
+    $router->get('/{id}', UserController::class . '@get');
+});
 
-$router->get('/posts', PostController::class . '@getAll');
-$router->get('/post/{id}', PostController::class . '@get');
+$router->group(['prefix' => 'posts'], function () use ($router) {
+    $router->get('/', PostController::class . '@getAll');
+    $router->get('/{id}', PostController::class . '@get');
+    $router->post('/', PostController::class . '@new');
+});
 
-$router->get('/comments', CommentController::class . '@getAll');
-$router->get('/comment/{id}', CommentController::class . '@get');
-
-/* new post-comment */
-$router->post('/post/new', PostController::class . '@new');
-$router->post('/comment/new', CommentController::class . '@new');
+$router->group(['prefix' => 'comments'], function () use ($router) {
+    $router->get('/', CommentController::class . '@getAll');
+    $router->post('/', CommentController::class . '@new');
+});
--- tests/PostTest.php
+++ tests/PostTest.php
@@ -4,6 +4,7 @@ class PostTest extends TestCase
 {
     public function testGetPosts()
     {
+        /* get all posts */
         $user = factory(App\User::class)->create();
         $post = factory(App\Post::class)->create([
             'user_id' => $user->id
@@ -14,51 +15,16 @@ class PostTest extends TestCase
         $this->seeInDatabase('posts', $post->toArray());
     }
 
-//    public function testC()
-//    {
-//        /* Creation test */
-//        $this->user = factory(App\User::class)->create();
-//        $this->post = factory(App\Post::class)->create([
-//            'user_id' => 1
-//        ]);
-//        $this->comment = factory(App\Comment::class)->create([
-//            'user_id' => 1,
-//            'post_id' => 1
-//        ]);
-//
-//        $user_controller = new UserController;
-//        $post_controller = new PostController;
-//        $comment_controller = new CommentController;
-//
-//        $tmpUser = $user_controller->get(1);
-//        $tmpPost = $post_controller->get(1);
-//        $tmpComment = $comment_controller->get(1);
-//
-//        try {
-//            $this->assertTrue($this->user->email == $tmpUser->email);
-//            $this->assertTrue($this->post->text == $tmpPost->text);
-//            $this->assertTrue($this->comment->text == $tmpComment->text);
-//        } catch (Exception $e) {
-//            echo 'Caught exception: ',  $e->getMessage(), "\n";
-//        }
-//    }
-//
-//    public function test2()
-//    {
-//        $user_controller = new UserController;
-//        $post_controller = new PostController;
-//        $comment_controller = new CommentController;
-//
-//        $tmpUser = $user_controller->get(1);
-//        $tmpPost = $post_controller->get(1);
-//        $tmpComment = $comment_controller->get(1);
-//
-//        try {
-//            $this->assertTrue($this->user->email == $tmpUser->email);
-//            $this->assertTrue($this->post->text == $tmpPost->text);
-//            $this->assertTrue($this->comment->text == $tmpComment->text);
-//        } catch (Exception $e) {
-//            echo 'Caught exception: ',  $e->getMessage(), "\n";
-//        }
-//    }
+    public function testGetPost()
+    {
+        /* get single post */
+        $user = factory(App\User::class)->create();
+        $post = factory(App\Post::class)->create([
+            'user_id' => $user->id
+        ]);
+        $this->get('/post/' . $post->id)
+            ->seeStatusCode(200)
+            ->seeJsonEquals($post->toArray());
+        $this->seeInDatabase('posts', $post->toArray());
+    }
 }
