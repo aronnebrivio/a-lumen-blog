e88d608bbf4f8e79850cd3923973be31066012cc
Aronne Brivio <aronne.brivio@studenti.unimi.it>
2018-04-11 10:57:13 +0200

    testCommentEdit not passing

--- tests/CommentTest.php
+++ tests/CommentTest.php
@@ -23,4 +23,33 @@ class Comment extends TestCase
         $this->json('GET','/comments/', ['post_id' => 1])
             ->seeStatusCode(404);
     }
+
+    function testCommentEdit()
+    {
+        $user = factory(App\User::class)->create();
+        $post = factory(App\Post::class)->create([
+            'user_id' => $user->id
+        ]);
+        $comment = factory(App\Comment::class)->create([
+            'user_id' => $user->id,
+            'post_id' => $post->id
+        ]);
+        $text = str_random(300);
+
+        $this->put('/comment/' . $comment->id, ["text" => $text])
+            ->seeStatusCode(401);
+
+        $this->get('/comment/' . $comment->id)
+            ->seeStatusCode(200)
+            ->seeJsonEquals($comment->toArray());
+
+        $this->actingAs($user);
+        $this->put('/comment/' . $comment->id, ["text" => $text])
+            ->seeStatusCode(200);
+
+        $comment->text = $text;
+        $this->get('/comment/' . $comment->id)
+            ->seeStatusCode(200)
+            ->seeJsonEquals($comment->toArray());
+    }
 }
