bf66d600bcfe8c67af74b66815901dcebbeb906f
Aronne Brivio <aronne.brivio@studenti.unimi.it>
2018-04-11 16:43:18 +0200

    testDuplicatedUser not passing

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -10,4 +10,16 @@ class UserTest extends TestCase
         $this->post('/users', ['email' => $email, 'password' => $pwd])
             ->seeStatusCode(200);
     }
+
+    public function testDuplicatedUser()
+    {
+        $email = 'test@example.com';
+        $pwd = 'password';
+
+        factory(App\User::class)->create([
+            'email' => $email
+        ]);
+        $this->post('/users', ['email' => $email, 'password' => $pwd])
+            ->seeStatusCode(409);
+    }
 }
