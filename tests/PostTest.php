a9f64a8647d3ed94dd838320d6049afe2a573adf
Aronne Brivio <aronne.brivio@studenti.unimi.it>
2018-04-12 17:46:50 +0200

    testPostEditValidation OK

--- tests/PostTest.php
+++ tests/PostTest.php
@@ -139,22 +139,23 @@ class PostTest extends TestCase
     {
         $user = factory(App\User::class)->create();
         $this->actingAs($user);
-        $post = factory(App\Post::class)->create();
 
+        $post = factory(App\Post::class)->create();
         $this->put('/posts/' . $post->id)
             ->seeStatusCode(200);
         $this->seeInDatabase('posts', ['user_id' => $user->id, 'text' => $post->text, 'title' => $post->title]);
 
+        $post = factory(App\Post::class)->create();
         $this->put('/posts/' . $post->id, ['title' => 'tit'])
             ->seeStatusCode(200);
         $this->seeInDatabase('posts', ['user_id' => $user->id, 'text' => $post->text, 'title' => 'tit']);
 
-
+        $post = factory(App\Post::class)->create();
         $this->put('/posts/' . $post->id, ['text' => 'txt'])
             ->seeStatusCode(200);
         $this->seeInDatabase('posts', ['user_id' => $user->id, 'text' => 'txt', 'title' => $post->title]);
 
-
+        $post = factory(App\Post::class)->create();
         $this->put('/posts/' . $post->id, ['title' => 'tit', 'text' => 'txt'])
             ->seeStatusCode(200);
         $this->seeInDatabase('posts', ['user_id' => $user->id, 'text' => 'txt', 'title' => 'tit']);
