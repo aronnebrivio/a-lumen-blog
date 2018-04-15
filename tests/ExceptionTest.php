975fc9cd1234fe6dcf568dcbed545b04fd35681e
Aronne Brivio <aroblu94@gmail.com>
2018-04-15 20:08:20 +0200

    testErrorEx not passing

--- tests/ExceptionTest.php
+++ tests/ExceptionTest.php
@@ -10,4 +10,13 @@ class ExceptionTest extends TestCase
         $this->assertEquals(405, $response->status());
         $this->assertEquals('Method Not Allowed.', $response->content());
     }
+
+    public function testErrorEx()
+    {
+        $user = factory(App\User::class)->create();
+        $response = $this->call('POST','/auth/', ['email' => $user->email]);
+
+        $this->assertEquals(422, $response->status());
+        $this->assertEquals('Unprocessable. Please provide all inputs and retry.', $response->content());
+    }
 }
