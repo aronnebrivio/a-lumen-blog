1a5cdfc359ccf7f6f4f47258adbcbc007b5457e1
Arone Brivio <aroblu94@gmail.com>
2019-01-17 20:49:12 +0100

    Fix configurazione CI

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -18,7 +18,7 @@
     echo 'Cloning repository'
     [ -d {{ $releases_dir }} ] || mkdir {{ $releases_dir }}
     git clone --depth 1 {{ $repository }} {{ $new_release_dir }}
-    cd {{ $releases_dir }}
+    cd {{ $new_release_dir }}
     git reset --hard {{ $commit }}
 @endtask
 
